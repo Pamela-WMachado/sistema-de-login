7e6467ebfa9771ab3216babb3c55093553506c3f
Pamela-WMachado <pamela.wehle@gmail.com>
2023-11-29 18:17:31 -0300

    desenvolvimento da autenticacao de login via jwt

--- app.js
+++ app.js
@@ -6,79 +6,151 @@ const jwt = require('jsonwebtoken')
 
 const app = express();
 
-//configurando middleware para leitura de JSON -> sem ele, a aplicação não aceita json nas suas requisiçoes e respostas
 app.use(express.json());
 
-// Models
 const User = require("./models/User")
 
+// PRIVATE ROUTE
+
+app.get("/user/:id", checkToken, async (req, res) => {
+
+    const id = req.params.id;
+
+    const user = await User.findById(id, "-password");
+
+    if(!user) {
+        return res.status(404).json({message: "Usuário não encontrado."});
+    }
+
+    res.status(200).json({ user })
+
+})
+
+function checkToken(req, res, next) {
+
+    const authHeader = req.headers["authorization"];
+
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token) {
+        return res.status(401).json({message: "Acesso negado."});
+    }
+
+    try {
+
+        const secret = process.env.SECRET;
+
+        jwt.verify(token, secret);
+
+        next();
+
+    } catch(error) {
+        return res.status(400).json({message: "Token inválido."});
+
+    }
+}
+
+
+// PUBLIC ROUTES
 app.get("/", (req, res) => {
     res.status(200).json({message: 'API no ar.'})
 });
 
-//Register User
-//utilizando um função assíncrona pois alguns recursos dependem de um tempo de resposta
 app.post('/auth/register', async(req, res) => {
 
-    //recebendo dados pelo body da requisiçao
     const { name, email, password, confirmPassword } = req.body;
 
-    //validation
     if (!name) {
-        return res.status(422).json({message: "Nome é obrigatório."})
+        return res.status(422).json({message: "Nome é obrigatório."});
     }
 
     if (!email) {
-        return res.status(422).json({message: "Email é obrigatório."})
+        return res.status(422).json({message: "Email é obrigatório."});
     }
 
     if (!password) {
-        return res.status(422).json({message: "Senha é obrigatória."})
+        return res.status(422).json({message: "Senha é obrigatória."});
     }
 
     if (!confirmPassword || confirmPassword !== password ) {
-        return res.status(422).json({message: "Senhas precisam ser iguais."})
+        return res.status(422).json({message: "Senhas precisam ser iguais."});
     }
 
-    //check if user exists 
-    //query do banco de dados
-    //model nos concede a metodos do mongoose e podemos procurar no banco de dados se esse mail ja foi cadastrado
     const userExists = await User.findOne({ email: email});
 
     if (userExists) {
         return res.status(422).json({ message: "Email já cadastrado. Por favor, insira um email válido."})
     }
 
-    //create pw -> add mais digitos a uma senha para torna-la mais segura
     const salt = await bcrypt.genSalt(12);
-    //password hash baseada na senha que o user informa
+
     const pwHash = await bcrypt.hash(password, salt);
 
-    //create user -> instancio uma nova classe utilizando a que importei (User)
     const user = new User({
         name,
         email,
-        password,
+        password: pwHash,
     })
 
     try {
         await user.save();
 
-        res.status(200).json({message: "Usuário criado com sucesso."})
+        res.status(200).json({message: "Usuário criado com sucesso."});
 
     } catch(error) {
-        res.status(500).json({message: error + "Falha no cadastro."})
+        return res.status(500).json({message: error + "Falha no cadastro."});
     }
 
+})
+
+
+app.post("/auth/login", async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(422).json({message: "Informe seu email."});
+    }
+
+    if (!password) {
+        return res.status(422).json({message: "Informe sua senha."});
+    }
+
+    const user = await User.findOne({ email: email});
+
+    if(!user) {
+        return res.status(404).json({ message: "Usuário não encontrado."});
+    }
+
+    const checkPw = await bcrypt.compare(password, user.password);
+
+    if (!checkPw) {
+        return res.status(422).json({ message: "Senha incorreta."});
+    }
+
+    try {
+
+        const secret = process.env.SECRET;
+
+        //enviar o token para testá-lo
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            secret,
+        )
+        res.status(200).json({message: "Autenticação realizada com sucesso.", token})
+
+    } catch(err) {
+        return res.status(500).json({message: "Não foi possível efetuar o login. Tente novamente."})
+    }
 })
 
 //Credentials
 const dbUser = process.env.DB_USER
 const dbPW = process.env.DB_PASS
 
-//snipet que disponibiliza a aplicação somente após a conexão com o banco
-//função connect do mongoose, passando o ip cadastrado
-//              -> promise based -> then() sucesso catch() erro
+
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPW}@cluster0.52fbwto.mongodb.net/?retryWrites=true&w=majority`).then(() => {
     app.listen(3000)
     console.log("Conectado ao banco.")
